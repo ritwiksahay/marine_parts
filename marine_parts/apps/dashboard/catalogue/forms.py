da21c6c02beac0f086da1d860ed60dcaacfba564
franpad8 <franpad8@gmail.com>
2018-01-16 16:14:04 -0400

    Creacion Vista de Componentes

--- marine_parts/apps/dashboard/catalogue/forms.py
+++ marine_parts/apps/dashboard/catalogue/forms.py
@@ -33,21 +33,23 @@ class CategoryForm(base_form.CategoryForm):
     def clean(self, *args, **kwargs):
         """Category Form Validation."""
         super(CategoryForm, self).clean(*args, **kwargs)
-        diagram = self.instance.diagram_image or self.cleaned_data['diagram_image']
+        diagram = self.instance.diagram_image
         # Only Category Leafs can have a diagram image
         if self.instance.has_children() and diagram:
             raise ValidationError(
                 _("A non leaf category cannot have a diagram image")
             )
 
-    def save(self):
+    def save(self, commit=True):
         """Override Category Form Save Method."""
         instance = super(CategoryForm, self).save(commit=False)
 
         # When adding a new category, the parent cannot have a diagram image
-        parent = instance.get_parent()
-        if parent.diagram_image:
-            parent.diagram_image = None
-        parent.save()
+        # If it is root, it doesn't make sense to check for parent
+        if not instance.is_root():
+            parent = instance.get_parent()
+            if parent.diagram_image:
+                parent.diagram_image = None
+            parent.save()
         instance.save()
         return instance
