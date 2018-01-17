4d0d95aaabc5b1dc25fd823ed4deb9c9b243278f
Franco Nori <francong2@gmail.com>
2018-01-17 14:19:06 -0400

    Settings

--- marine_parts/settings/settings.py
+++ marine_parts/settings/settings.py
@@ -67,6 +67,7 @@ DJANGO_APPS = [
 INSTALLED_APPS = THIRD_PARTY_APPS + DJANGO_APPS + SYSTEM_APPS \
     + get_core_apps([
         'marine_parts.apps.promotions',
+        'marine_parts.apps.customer',
     ])
 
 AUTH_USER_MODEL = 'users.User'
