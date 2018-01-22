e173b67694e77082218e5d7e45f3d9f279924ace
Daniel Leones <dleones@ubicutus.com>
2018-01-22 09:55:03 -0400

    Eliminar datos insignificantes.

--- marine_parts/apps/users/models.py
+++ marine_parts/apps/users/models.py
@@ -5,27 +5,4 @@ from oscar.apps.customer.abstract_models import AbstractUser
 
 # Extensiones a Users definidos en Oscar
 class User(AbstractUser):
-
-    def get_full_name(self):
-        full_name = '%s %s' % (self.last_name.upper(), self.first_name)
-        return full_name.strip()
-
-
-class Customer(models.Model):
-    birthday = models.DateField()
-    # user = models.ForeignKey(User)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-
-class Store(models.Model):
-    name = models.CharField(max_length=100)
-    country = CountryField()
-
-class Note(models.Model):
-    user = models.ForeignKey(User)
-    pub_date = models.DateTimeField()
-    title = models.CharField(max_length=200)
-    body = models.TextField()
-
-    def __str__(self):
-        return self.title
+    pass
