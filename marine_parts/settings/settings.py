9adf16b686f7b38fe25c166fc0d408aeedf6ae26
Franco Nori <francong2@gmail.com>
2018-02-22 15:25:12 -0400

    Email setup

--- marine_parts/settings/settings.py
+++ marine_parts/settings/settings.py
@@ -319,7 +319,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'parts@marineparts.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'M@rine0470'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 SCRAPPER_ROOT = os.path.join(BASE_DIR, 'parts_scrapper/')
