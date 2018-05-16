43bede72ce9551efb40c96b413ceb8601c2dd0ce
Franco Nori <francong2@gmail.com>
2018-05-16 09:35:55 -0400

    Minor changes and refactor

--- marine_parts/apps/categorizador/prods_excel.py
+++ marine_parts/apps/categorizador/prods_excel.py
@@ -4,14 +4,24 @@ from django.db import transaction
 
 
 def exec_excel_load(path_arch, cat, cat_base, nom_manufac, nom_origin):
-    return excel_load(cat, DBAccess(cat_base), ExcelHandler().leer(path_arch), nom_manufac, nom_origin)
+    return excel_load(
+        cat,
+        DBAccess(cat_base),
+        ExcelHandler().leer(path_arch),
+        nom_manufac,
+        nom_origin
+    )
+
 
 @transaction.atomic
 def excel_load(cat, db_marine_parts, ls_prods, nom_manufac, nom_origin):
     nro = 0
     cat = db_marine_parts.crear_categoria(['', cat], None)
+
     for row in ls_prods:
-        db_marine_parts.crear_prods(cat, True, row['Name'], row['SKU'], nom_manufac,
-            nom_origin, '', row['Price'], row['SalePrice'], row['Cost'])
+        db_marine_parts.crear_prods(
+            cat, True, row['Name'], row['SKU'], nom_manufac,
+            nom_origin, '', row['Price'], row['SalePrice'], row['Cost']
+        )
         nro += 1
     return nro
